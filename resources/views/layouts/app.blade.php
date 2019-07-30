4df12d23e3195213185cc8aab6fd44f61a524a00
gabrielg21 <gabo21gonzalez@gmail.com>
2019-07-30 01:14:02 -0600

    Vista de configuaración de usuario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,12 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ config('app.name', 'Laravel') }}</title>
-  <!-- Bootstrap css --> 
+  <!-- Bootstrap css -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <!-- Estilos barra lateral y contenedor de mapa -->
   <link rel="stylesheet" type="text/css" href="{{ asset('css/estilos_barra.css')}}">
   <!-- Iconos -->
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" /> 
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
   <!-- Leafleat css -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
@@ -28,13 +28,13 @@
     </button>
     <a class="navbar-brand" href="#">
         <img src="{{ asset('img/Xmaphn.png')}}" width="100" height="50" class="d-inline-block align-top" alt="">
-        
-            
+
+
     </a>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
 
-            <!-- This menu is hidden in bigger devices with d-sm-none. 
+            <!-- This menu is hidden in bigger devices with d-sm-none.
            The sidebar isn't proper for smaller screens imo, so this dropdown menu can keep all the useful sidebar itens exclusively for smaller screens  -->
             <li class="nav-item dropdown d-sm-block d-md-none">
                 <a class="nav-link dropdown-toggle" href="#" id="smallerscreenmenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -50,8 +50,8 @@
     </div>
     <div class="profile">
       <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <img src="{{ asset('img/3BCARS.png')}}"  id="circle">
-        <span class="color_nombre">Juan soler</span>
+        <img src="{{ asset('img/user.jpg')}}"  id="circle">
+        <span class="color_nombre">Bruce Wayne</span>
       </a>
       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
         <a class="dropdown-item" href="#">Mi perfil</a>
@@ -71,7 +71,7 @@
         <!-- d-* hiddens the Sidebar in smaller devices. Its itens can be kept on the Navbar 'Menu' -->
         <!-- Bootstrap List Group -->
         <ul class="list-group sticky-top sticky-offset">
-        	@yield('Items_sidebar')        
+        	@yield('Items_sidebar')
         </ul>
         <!-- List Group END-->
         @yield('banner')
@@ -139,9 +139,9 @@ var temp=[];
           //show:false
           }).addTo(mymap);
         }
-        
+
       }
-      
+
     }
   mymap.on('click', onMapClick);
   </script>
--- resources/views/user/config.blade.php
+++ resources/views/user/config.blade.php
@@ -1,6 +1,97 @@
 @extends('layouts.app_user')
 
+<style>
+    td, th {
+        text-align: left;
+        padding: 8px;
+    }
+</style>
+
 @section('Marco')
-    <h1>Config</h1>
-    <p>Seccion donde se encontra las configuraciones del usuario</p>
+<div class="container">
+    <div class="row" style="padding-top: 80px">
+        <div class="col-md-1 col-sm-1">
+        </div>
+        <div class="col-sm-2 col-md-2 col-lg-2 col-xl-2">
+            <img src="{{ asset('img/user.jpg')}}"  id="circle">
+        </div>
+        <div class="col-sm-3 col-md-3 col-lg-2 col-xl-2">
+            <button class="btn btn-large btn-primary">
+                Modificar foto de perfil
+            </button>
+        </div>
+    </div>
+    <div class="row" style="padding-top: 16px">
+       <div class="col-md-1">
+       </div>
+       <div class="col-sm-5 col-md-5 col-lg-3">
+            <h2>Inicio de sesión</h2>
+       </div>
+    </div>
+    <div class="row" style="padding-top: 15px">
+        <div class="col-md-1">
+        </div>
+        <table>
+            <tr>
+                <td>Correo Electrónico</td>
+                <td><span class="form-control">correo@dominio.com</span></td>
+                <td>
+                    <button class="btn btn-primary form-control">
+                        Editar
+                    </button>
+                </td>
+            </tr>
+            <tr>
+            <td>Contraseña</td>
+                <td><span class="form-control">**********</span></td>
+                <td>
+                    <button class="btn btn-primary form-control">
+                        Editar
+                    </button>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="row" style="padding-top: 16px">
+       <div class="col-md-1">
+       </div>
+       <div class="col-sm-5 col-md-5 col-lg-3">
+            <h2>Información</h2>
+       </div>
+    </div>
+    <div class="row" style="padding-top: 15px">
+        <div class="col-md-1">
+        </div>
+        <table>
+            <tr>
+                <td>Nombre de Usuario</td>
+                <td><input type="text" value="El Caballero de la noche" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Sexo</td>
+                <td>
+                    <select name="slc-sexo" id="slc-sexo" class="form-control">
+                        <option value="M">Masculino</option>
+                        <option value="F">Femenino</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+            <td>Municipio</td>
+            <td>
+                <select name="slc-municipio" id="slc-municipio" class="form-control">
+                    <option value="1">Distrito Central</option>
+                    <option value="2">San Pedro Sula</option>
+                </select>
+            </td>
+            </tr>
+            <tr>
+                <td>
+                <button class="btn btn-primary">Guardar</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+
 @endsection
