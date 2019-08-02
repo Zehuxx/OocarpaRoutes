1909a6cba92e878dfe8be2b697fbf9d2c3fc8a64
frost <denisadonis06@gmail.com>
2019-08-01 23:18:05 -0600

    reinicio del repositorio

--- resources/views/user/config.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app_user')
-
-@section('Marco')
-    <h1>Config</h1>
-    <p>Seccion donde se encontra las configuraciones del usuario</p>
-@endsection
--- resources/views/user/routes.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app_user')
-
-@section('Marco')
-    <h1>Rutas creadas</h1>
-    <p>Seccion para listar las rutas creadas</p>
-@endsection
