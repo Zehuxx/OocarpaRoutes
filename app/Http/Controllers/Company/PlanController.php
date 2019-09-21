d9b81097007a382d0bf2825bfff72373ef527df1
frost <denisadonis06@gmail.com>
2019-09-21 16:57:20 -0600

    Puliendo la optencion de planes

--- app/Http/Controllers/Company/PlanController.php
+++ app/Http/Controllers/Company/PlanController.php
@@ -16,7 +16,7 @@ class PlanController extends Controller
     {
 
         $planes = Plan::all();
-        $planBuyed = CompanyPlan::all()->where('company_id', Auth::user()->id)->first();
+        $planBuyed = CompanyPlan::all()->where('company_id', Auth::user()->id)->sortByDesc('created_at')->first();
 
         return view('company.planes', compact('planes', "planBuyed"));
     }
@@ -25,14 +25,13 @@ class PlanController extends Controller
         $planBuyed = CompanyPlan::all()->where('company_id', Auth::user()->id)->sortByDesc('created_at')->first();
 
         if ($planBuyed != NULL) {
-            if (Carbon::now() > (new Carbon($planBuyed->end_date))) {
+            if (Carbon::now() > (new Carbon($planBuyed->end_date)) || $planBuyed->Plan->name == 'Gratis') {
                 $plan = new CompanyPlan();
                 $plan->company_id = new ObjectID(Auth::user()->id);
                 $plan->plan_id = new ObjectID($id);
                 $plan->start_date = Carbon::now()->toDateTimeString();
                 $plan->end_date = Carbon::now()->addDay(7)->toDateTimeString();
                 $plan->save();
-
             } else {
 
             }
